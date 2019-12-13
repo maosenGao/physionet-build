53bf06db85cc08325b340f5428439910680e8b84
Tom Pollard <tpollard@mit.edu>
2019-12-13 15:51:14 -0500

    PEP8. Minor changes to wording.

--- physionet-django/console/utility.py
+++ physionet-django/console/utility.py
@@ -14,8 +14,9 @@ from django.conf import settings
 import logging
 
 LOGGER = logging.getLogger(__name__)
-ROLES = ['roles/storage.legacyBucketReader', 'roles/storage.legacyObjectReader',
-    'roles/storage.objectViewer']
+ROLES = ['roles/storage.legacyBucketReader',
+         'roles/storage.legacyObjectReader',
+         'roles/storage.objectViewer']
 
 
 def check_bucket_exists(project, version):
@@ -44,15 +45,16 @@ def create_bucket(project, version, title, protected=False):
     bucket = storage_client.bucket(bucket_name)
     bucket.iam_configuration.bucket_policy_only_enabled = True
     bucket.patch()
-    LOGGER.info("Created bucket {0} for project {1}".format(bucket_name.lower(), project))
+    LOGGER.info("Created bucket {0} for project "
+                "{1}".format(bucket_name.lower(), project))
     if not protected:
         make_bucket_public(bucket)
         LOGGER.info("Made bucket {0} public".format(bucket_name.lower()))
     else:
         remove_bucket_permissions(bucket)
         group = create_access_group(bucket, project, version, title)
-        LOGGER.info("Purged permissions from bucket {0} and granted {1} read access".format(
-            bucket_name.lower(), group))
+        LOGGER.info("Removed permissions from bucket {0} and granted {1} "
+                    "read access".format(bucket_name.lower(), group))
 
 
 def get_bucket_name(project, version):
@@ -61,8 +63,10 @@ def get_bucket_name(project, version):
     """
     current_site = Site.objects.get_current()
     production_site = Site.objects.get(id=3)
+
     if current_site.domain != production_site.domain:
-        return 'testing-delete.{0}-{1}.{2}'.format(project, version, production_site.domain)
+        return 'testing-delete.{0}-{1}.{2}'.format(project, version,
+                                                   production_site.domain)
     return '{0}-{1}.{2}'.format(project, version, production_site.domain)
 
 
@@ -72,8 +76,10 @@ def get_bucket_email(project, version):
     """
     current_site = Site.objects.get_current()
     production_site = Site.objects.get(id=3)
+
     if current_site.domain != production_site.domain:
-        return 'testing-delete-{0}-{1}@{2}'.format(project, version, production_site.domain)
+        return 'testing-delete-{0}-{1}@{2}'.format(project, version,
+                                                   production_site.domain)
     return '{0}-{1}@{2}'.format(project, version, production_site.domain)
 
 
@@ -94,15 +100,19 @@ def remove_bucket_permissions(bucket):
     """
     policy = bucket.get_iam_policy()
     to_remove = []
+
     for role in policy:
         if role != 'roles/storage.legacyBucketOwner':
             for member in policy[role]:
                 to_remove.append([role, member])
+
     for item in to_remove:
         policy[item[0]].discard(item[1])
+
     if to_remove:
         bucket.set_iam_policy(policy)
-        LOGGER.info("Removed all read permissions from bucket {}".format(bucket.name))
+        LOGGER.info("Removed all read permissions from "
+                    "bucket {}".format(bucket.name))
 
 
 def create_access_group(bucket, project, version, title):
@@ -116,6 +126,7 @@ def create_access_group(bucket, project, version, title):
     email = get_bucket_email(project, version)
     service = create_directory_service(settings.GCP_DELEGATION_EMAIL)
     production_domain = Site.objects.get(id=3)
+
     # Get all the members of the Google group
     try:
         outcome = service.groups().list(domain=production_domain).execute()
@@ -123,23 +134,25 @@ def create_access_group(bucket, project, version, title):
             creation = service.groups().insert(body={
                 'email': email,
                 'name': '{0} v{1}'.format(title, version),
-                'description': 'Group that handles the access for the PhysioNet \
-                    project {0} version {1}'.format(title, version)}).execute()
+                'description': "Access group for the project {0} version "
+                               "{1}.".format(title, version)}).execute()
             if creation['kind'] != 'admin#directory#group':
-                LOGGER.info("There was an error {0} creating the group {1}".format(creation, email))
+                LOGGER.info("Error {0} creating the "
+                            "group {1}.".format(creation, email))
                 return False
-            LOGGER.info("The access group {0} was successfully created".format(email))
+            LOGGER.info("Access group {0} was created.".format(email))
             if update_access_group(email):
-                LOGGER.info("The access group {0} was successfully updated".format(email))
+                LOGGER.info("Access group {0} was updated.".format(email))
         else:
-            LOGGER.info("The access group {0} was previosly created".format(email))
+            LOGGER.info("Access group {0} already exists.".format(email))
     except HttpError as e:
         if json.loads(e.content)['error']['message'] != 'Member already exists.':
-            LOGGER.info('Unknown error {0} creating the group {1} for access to \
-                {1}'.format(e.content, email, project))
+            LOGGER.info("Error {0} creating the access group {1} for "
+                        "{1}.".format(e.content, email, project))
             raise e
         else:
-            LOGGER.info("The access group {0} was previosly created".format(email))
+            LOGGER.info("Access group {0} already exists.".format(email))
+
     return email
 
 
@@ -150,16 +163,19 @@ def update_access_group(email):
     Returns:
         bool: False if there was an error or change to the API. True otherwise.
     """
-    service = create_directory_service(settings.GCP_DELEGATION_EMAIL, group=True)
+    service = create_directory_service(settings.GCP_DELEGATION_EMAIL,
+                                       group=True)
     update = service.groups().update(groupUniqueId=email, body={
         'allowExternalMembers': 'true',
         'whoCanPostMessage': 'ALL_OWNERS_CAN_POST',
         'whoCanModerateMembers': 'OWNERS_AND_MANAGERS',
         'whoCanJoin': 'INVITED_CAN_JOIN'}).execute()
+
     if update['kind'] != 'groupsSettings#groups':
-        LOGGER.info("There was an error {0} setting the permissions of the group\
-            {1}".format(update, email))
+        LOGGER.info("Error {0} setting the permissions for group "
+                    "{1}".format(update, email))
         return False
+
     return True
 
 
@@ -173,44 +189,48 @@ def add_email_bucket_access(project, email, group=False):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(project.gcp.bucket_name)
     policy = bucket.get_iam_policy()
+
     if group:
         for role in ROLES:
             policy[role].add('group:'+email)
     else:
         for role in ROLES:
             policy[role].add('user:'+email)
+
     try:
         bucket.set_iam_policy(policy)
         LOGGER.info("Added email {0} to the project {1} access list".format(
             email, project))
         return True
-    except BadRequest: 
-        LOGGER.info("There was an error on the request. The email {} was ignored.".format(
-            email))
+    except BadRequest:
+        LOGGER.info("Error in the request. The email {} was "
+                    "ignored.".format(email))
         return False
 
 
 def upload_files(project):
     """
     Upload files to a bucket. Gets a list of all the files under the project
-    root directory, then sends each file individually. Check storage size to 
+    root directory, then sends each file individually. Check storage size to
     confirm that the zip file was created.
     """
     file_root = project.file_root()
     subfolders_fullpath = [x[0] for x in walk(file_root)]
     storage_client = storage.Client()
     bucket = storage_client.get_bucket(project.gcp.bucket_name)
+
     for indx, location in enumerate(subfolders_fullpath):
         chdir(location)
         files = [f for f in listdir('.') if path.isfile(f)]
         for file in files:
-            temp_dir = location.replace(file_root,'')
+            temp_dir = location.replace(file_root, '')
             if temp_dir != '':
                 blob = bucket.blob(path.join(temp_dir, file)[1:])
                 blob.upload_from_filename(file)
             else:
                 blob = bucket.blob(file)
                 blob.upload_from_filename(file)
+
     if project.compressed_storage_size:
         zip_name = project.zip_name()
         chdir(project.project_file_root())
@@ -236,7 +256,8 @@ def create_directory_service(user_email, group=False):
         settings.GCP_SECRET_KEY,
         scopes=['https://www.googleapis.com/auth/admin.directory.group',
                 'https://www.googleapis.com/auth/apps.groups.settings'])
-    # This requires the email used to delegate the credentials to the serivce account
+
+    # The email for delegating credentials to the service account is required.
     credentials = credentials.create_delegated(user_email)
     if group:
         return build('groupssettings', 'v1', credentials=credentials)
